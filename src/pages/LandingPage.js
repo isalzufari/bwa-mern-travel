b2518e49690e78a11eaad948b1221656b8c5a975
isalzufari <isalzufari@gmail.com>
2022-07-16 07:23:53 +0700

    Section Most Picked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,21 +1,29 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from 'parts/Header';
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
-import landingPage from '../json/landingPage.json'
+import landingPage from "../json/landingPage.json";
 
 class LandingPage extends Component {
   constructor(props) {
     super(props);
+    this.refMostPicked = React.createRef();
   }
 
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
-    )
+    );
   }
 }
 
