261a770b0c65639e1dc0eb7c99fdf9100a2cb30f
isalzufari <isalzufari@gmail.com>
2022-07-16 08:11:32 +0700

    Section Categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 import landingPage from "../json/landingPage.json";
 
@@ -17,10 +18,16 @@ class LandingPage extends Component {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <Hero 
+          refMostPicked={this.refMostPicked} 
+          data={landingPage.hero} 
+        />
         <MostPicked
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
+        />
+        <Categories 
+          data={landingPage.categories}
         />
       </>
     );
